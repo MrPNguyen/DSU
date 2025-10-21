7b5e3ba8554c9dd26eec70f6c7417d5ee6b3cd67
Philip Nguyen <yetrexgamer@gmail.com>
2025-10-21 12:31:58 +0200

    Two problems done... four to go... kill me

--- Invaders/Program.cs
+++ Invaders/Program.cs
@@ -40,10 +40,10 @@ namespace Invaders
 }
 
 //Problems:
-//Fix Game reset Crash
+//Fix Game reset Crash. Done
+//Fix Game over buttons not highlighting. Done
 //Fix high score in game over screen
 //Fix high score list in main menu
 //Fix my life
 //Fix audio sometimes cuts when going from pause menu to main menu
-//Fix Game over buttons not highlighting
 //
